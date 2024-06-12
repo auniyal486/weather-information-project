dab44d3b14a3104c5cd9e4f477b4ad4fa2e4fce6
Abhishek Uniyal <64672076+auniyal486@users.noreply.github.com>
2024-06-12 17:56:16 +0530

    test comment for action

--- weather_app/apps.py
+++ weather_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
+#hello this is testing commenting
 class WeatherAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "weather_app"
